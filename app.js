21a8bcb2c29a0a4a10c990243b812f387885f107
saadshiskin <saadshiskin@gmail.com>
2024-11-24 00:02:44 +0500

    chart js implementation in html

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+// var age = 21;
+
+
+// switch (true) {
+//   case (age >= 15 && age <= 20):
+//     document.write("you are eligible ");
+//     break;
+//     case (age <= 21 && age <= 30):
+//     document.write("you are not eligible");
+//     break;
+//   default:
+//     document.write("enter the valid Age")
+//     break;
+// }
+
+
+// var per = 33;
+
+// if(per >= 80 && per <= 100){
+//   document.write("you are in merit");
+// }else if(per >= 60 && per < 80){
+//   document.write("you are in 1st divison");
+// }else if(per >= 45 && per < 60){
+//   document.write("you are in 2 division");
+// }else if(per >= 33 && per < 45){
+//   document.write("you are in third divison");
+// }else if(per < 33){
+//   document.write("you are fail");
+// }else{
+//   document.write("please enter valid percentage")
+// }
+
+
+
+// const fruits = ["Apple", "Banana", "Cherry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`Fruit: ${fruits[i]}`);
+// }
+
+
+var a = ["saad", "shoaib", "ahmer", "masood"]; 
